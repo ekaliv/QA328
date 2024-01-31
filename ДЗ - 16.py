71ec67309388ddae1f33692ce73c8976b833103d
ekaliv <154007574+ekaliv@users.noreply.github.com>
2024-01-31 14:53:23 +0300

    Add files via upload

---
+++ ДЗ - 16.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, title=None, author=None, year=None):
+        self.title = title
+        self.author = author
+        self.year = year
+    def display(self):
+        text = ""
+        if self.title != None: text = text + self.title[0:30] + " "
+        if self.author != None: text = text + self.author[0:20] + " "
+        if self.year != None: text = text + str(self.year)[0:5] + ""
+        print(text)
+
+book = Book('War and Peace.', 'Leo Tolstoy.', 1828)
+book.display()
